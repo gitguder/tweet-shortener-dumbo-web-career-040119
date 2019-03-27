e8c87c25d931bb25233446d9cc35352b24027a07
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 04:44:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 
 def word_substituter(tweet)
   tweet.split.collect do |word|
-    if word == dictionary.keys
+    if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
       word
