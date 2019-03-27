a3df40f2e09ca9d38f1e89159605777152f9fda1
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 04:26:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
+require 'pry'
 # Write your code here.
+def dictionary
+ {
+   "hello" => "hi",
+   "to, two, too" => "2",
+   "for, four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+ }
+
+end
+
+def word_substituter(tweet)
+  array = tweet.split(" ")
+
+    array.each do |word|
+      if word == dictionary.keys
+        binding.pry
+        word = dictionary.value
+      end
+    end
+  array.join(" ")
+end
