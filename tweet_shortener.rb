830ce87431915c6c41c06282f5e9e6828bd2c252
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 05:01:41 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
+
 # Write your code here.
+def dictionary
+ {
+   "hello" => "hi",
+   "too" => "2",
+   "to" => "2",
+   "two" => "2",
+   "four" => "4",
+   "for" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+ }
+end
+
+def word_substituter(tweet)
+  tweet.split.collect {|word| dictionary.keys.include?(word.downcase) ? word = dictionary[word.downcase] : word}.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each { |tweet| puts word_substituter(tweet) }
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  word_substituter(tweet).length > 140 ? word_substituter(tweet)[0..136] + "..." : tweet
+end
