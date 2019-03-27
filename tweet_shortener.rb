fc496acdc4b3785b83fe8fcb56425f613d77922b
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 05:00:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,11 +24,7 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.length > 140
-    word_substituter(tweet)
-  else
-    tweet
-  end
+  tweet.length > 140 ? word_substituter(tweet) : tweet
 end
 
 def shortened_tweet_truncator(tweet)
